02925a85b1935d47bd64a5f4654ec876ffdafeda
Alec Chapman <abchapman93@gmail.com>
2022-07-28 18:40:22 -0600

    Working on week 1

--- quizzes/select_multiple_quiz.py
+++ quizzes/select_multiple_quiz.py
@@ -5,8 +5,10 @@ from .quiz import Quiz
 class SelectMultipleQuiz(Quiz):
     def __init__(self, description="", answer=tuple(), options=tuple(), show_answer=False, shuffle_answer=True):
         options = list(options)
-        if answer not in options:
-            options.append(answer)
+        answer = set(answer)
+        for a in answer:
+            if a not in options:
+                options.append(a)
         super().__init__(description, answer, show_answer)
 
         if shuffle_answer:
@@ -32,3 +34,23 @@ class SelectMultipleQuiz(Quiz):
 
             ]
         )
+
+    def _submit_answer(self, change):
+        self.submitted = self._options.value
+        self._validate_answer()
+
+    def _validate_answer(self):
+        # If there's already a response, remove it
+        submitted = set(self.submitted)
+        actual = set(self.answer)
+
+        # Check if their are any differences
+        if submitted == actual:
+            self.response = "That is correct!"
+        else:
+            self.response = "That is incorrect."
+            if self.show_answer:
+                self.response += f" Expected '{self.answer}'. Got '{self.submitted}'"
+
+        self._response.value = self.response
+
