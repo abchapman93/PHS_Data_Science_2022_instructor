f63c67289f4cb3947e33239a6ae0158bdf84f94a
Alec Chapman <abchapman93@gmail.com>
2022-08-05 15:08:21 -0600

    Finish Module 2

--- quizzes/module_2_quizzes.py
+++ quizzes/module_2_quizzes.py
@@ -95,3 +95,137 @@ hint_severity_plot = QuizHint(
     ]
 )
 
+ethnicity_descr_hint = QuizHint(hints=[
+    widgets.HTML(
+        """Your plot should look something like:</br>
+        <img src="media/ethnicity_descr_counts.png"></img>
+        """
+    )
+])
+
+age_at_discharge_mc_quiz = MultipleChoiceQuiz("What are the mean and median ages at discharge?",
+                                              answer="70.29; 73.41",
+                                              options=["73.41; 70.29", "61.14; 105.42"],
+                                              )
+
+ethnicity_descr_hint = QuizHint(hints=[
+
+
+    widgets.HTML(
+        """Your plot should look something like:</br>
+        <img src="media/ethnicity_descr_counts.png"></img>
+        """
+    )
+])
+
+age_at_discharge_mc = MultipleChoiceQuiz("What are the mean and median ages at discharge?",
+                                         answer="70.29; 73.41",
+                                         options=["73.41; 70.29", "61.14; 105.42"],
+
+                                         )
+
+age_at_discharge_viz_hint = QuizHint(description="Visualization hints",
+        hints=[
+            widgets.HTML("""One option is a histogram:</br>
+            <img src="./media/age_at_discharge_hist.png"></img>
+            """),
+            widgets.HTML("""One option is a histogram:</br>
+            <img src="./media/age_at_discharge_boxplot.png"></img>
+            """),
+        ]
+        )
+
+prop_pt_mortality_quiz = MultipleChoiceQuiz(description="What proportion of patients died in the hospital?",
+            answer=0.33, options=[0.66, 0.43, 0.1])
+
+age_at_discharge_mortality_hint = QuizHint(description="Visualization hints",
+        hints=[
+            widgets.HTML("""Your plot should look something like:</br>
+            <img src="./media/age_at_discharge_proportions.png"></img>
+            """)
+        ]
+        )
+
+mortality_cols_quiz = SelectMultipleQuiz(answer=("time_discharge_to_death", "disch_dt", "dod"),
+                  options=('subject_id', 'hadm_id', 'disch_dt', 'dod', 'sex', 'ethnicity_descr',
+       'age_at_discharge', 'age_at_discharge_binned', 'pna',
+       'time_discharge_to_death', 'mortality_30_day'), shuffle_answer=False)
+
+in_hospital_mortality_hint = QuizHint(
+    hints=[
+        widgets.HTML("You could either represent this variable as an integer (0 or 1) or a boolean (True or False)."),
+        widgets.HTML("If you have a Series and run `series1 == value`, you'll get back a Series of booleans indicating whether that row has that value."),
+    ]
+)
+
+def test_in_hospital_mortality_validation_func(actual_df):
+    import pandas as pd
+    import numpy as np
+    # Check that it's a Series
+    if not isinstance(actual_df, pd.DataFrame):
+        print(f"Incorrect value passed in. Data type should be pd.DataFrame, got {type(actual_df)}")
+        return
+
+    # Check that it has a column
+    try:
+        actual = actual_df["in_hospital_mortality"]
+    except KeyError:
+        print("Your DataFrame doesn't have a column named 'in_hospital_mortality'")
+        return
+
+    # Check that the column contains an acceptable datatype
+    if actual.dtype not in ("int64", "float64", "bool"):
+        print(f"Incorrect. 'in_hospital_mortality' should contain int, float, or bool, not {str(actual.dtype)}")
+        return
+
+    if len(actual_df) != 5074:
+        print(f"Expected 5074 rows, got {len(actual_df)}")
+        return
+
+    # Check that the mean value is expected
+    actual_mean = np.round(actual.mean(), 4)
+    if actual_mean != 0.3285:
+        print(f"Mean value of 'in_hospital_mortality' wasn't what was expected. Expected 0.3285, got {actual_mean}")
+        return
+
+    print("That is correct!")
+
+
+age_at_discharge_mortality_quiz = SelectMultipleQuiz("What are the two age groups wwith the highest proportion of in-hospital mortality?",
+                   answer=("<18", "91+"),
+                   options=('<18', '18-35', '36-65', '66-90', '91+',),
+                   shuffle_answer=False
+                  )
+
+test_in_hospital_mortality = ValueTest(validation_func=test_in_hospital_mortality_validation_func)
+
+pna_prop = MultipleChoiceQuiz("What is the proportion of patients had pneumonia? Round to 4 decimal points.",
+            answer=0.2, options=[0.8, 0.33, 0.66])
+
+time_to_death_viz_hint = QuizHint(description="Some hints for visualizing time to death.",
+    hints=[
+        widgets.HTML("""One option is a boxplot</br>:
+    <img src="./media/time_discharge_to_death_boxplot.png"></img>
+    """),
+        widgets.HTML("""Another option is a histogram:</br>:
+    <img src="./media/time_to_death_hist.png"></img>
+    """)
+          ]
+)
+
+time_to_death_mean_quiz = MultipleChoiceQuiz("What are the mean number of days from discharge to death for pneumonia positive and negative patients?",
+             answer="254.1; 398.9",
+             options=["398.9; 254.1", "35.0; 113.5", "113.5; 35.0"]
+            )
+
+in_hospital_mortality_rr_quiz = FreeTextTest(
+    "What is the relative risk of in-hospital mortality for pneumonia positive vs pneumonia negative patients?</br>Round to 2 decimals.",
+    answer=1.27
+
+)
+
+in_hospital_mortality_rr_interpretation_quiz = MultipleChoiceQuiz("The interpretation of the relative risk is:</br>Patients with pneumonia are ___ ___ than patients without pneumonia to die in the hospital.",
+                   answer=" 1.27 times more likely",
+                   options=["1.27 times less likely", "Similarly likely"]
+                  )
+
