6b18a37047a795179ddcfe173bf7c6b608e6250b
Alec Chapman <abchapman93@gmail.com>
2022-08-07 12:01:10 -0600

    Working on module 3

--- quizzes/module_3_quizzes.py
+++ quizzes/module_3_quizzes.py
@@ -116,8 +116,70 @@ def test_query_dob_descending(actual):
     if tuple(actual["subject_id"]) != (37, 78, 56):
         print(
             f"Incorrect. Your dataframe should have subject ids in the order (37, 78, 56), not {tuple(actual['subject_id'])}")
+    print("That is correct!")
 
+test_query_dob_descending = ValueTest(validation_func=test_query_dob_descending)
+
+hint_d_patients_unq = QuizHint(
+    hints=[
+        widgets.HTML("""One option is to group by subject_id, count the number of rows per subject_id using .size(), and then take the max:</br>
+        <p style="font-family:courier";>df_patients.groupby("subject_id").size().max()</br>>>> 1</p>"""),
+        widgets.HTML("""Another option is to check whether the length of the set of subject_ids (which is deduplicated)
+        equals the length of the entire table:</br>
+        <p style="font-family:courier";>len(set(df_patients["subject_id"])) == len(df_patients)</br>>>> True</p>"""),
+    ]
+)
+
+
+def validate_df_patients_renamed_validation_func(actual):
+    import pandas as pd
+    if not isinstance(actual, pd.DataFrame):
+        print(f"Incorrect. df should be a pandas DataFrame, not {type(actual)}")
+        return
+    if len(actual) != 10:
+        print(f"Incorrect. df should have 10 rows. Your dataframe had {len(actual)}.")
+        return
+    if {"date_of_birth", "date_of_death"}.difference(set(actual.columns)):
+        print(
+            f"Incorrect. Your dataframe should have columns 'date_of_birth' and 'date_of_death'. Your dataframe has columns {list(actual.columns)}")
+        return
     print("That is correct!")
 
 
-test_query_dob_descending = ValueTest(validation_func=test_query_dob_descending)
+validate_df_patients_renamed = ValueTest(validation_func=validate_df_patients_renamed_validation_func)
+
+hint_age_in_years = QuizHint(
+    hints=[widgets.HTML("""In python, the following could would convert a variable called age_at_death_days into years:</br>
+    <p style="font-family:courier";>age_at_death_days / 365</p>""")]
+)
+
+def test_age_at_death_validation_func(actual):
+    import pandas as pd
+    if not isinstance(actual, pd.DataFrame):
+        print(f"Incorrect. df should be a pandas DataFrame, not {type(actual)}")
+        return
+    if len(actual) != 4000:
+        print(f"Incorrect. df should have 4000 rows. Your dataframe had {len(actual)}.")
+        return
+    if "age_at_death" not in set(actual.columns):
+        print(
+            f"Incorrect. Your dataframe should have columns 'age_at_death'. Your dataframe has columns {list(actual.columns)}")
+        return
+    # Test value for a patient
+    expected_value = 77.2247
+    actual_value = actual.query("subject_id == 3").iloc[0]["age_at_death"]
+    if expected_value != actual_value:
+        print(f"Incorrect age at death for subject 3. Expected {expected_value}, got {actual_value}")
+        return
+    print("That is correct!")
+test_age_at_death = ValueTest(validation_func=test_age_at_death_validation_func)
+
+quiz_prop_hospital_expire_flg = MultipleChoiceQuiz(answer=0.41,
+                                                  options=[0.59, 0.23, 1653])
+
+hint_boxplot_age_death_by_sex = QuizHint(hints=[
+    widgets.HTML("""One option is to use seaborn to create a visualization like this one:</br>
+    <img src="./media/output_boxplot_age_death_sex.png" width="50%"></img>
+    """)
+])
+
