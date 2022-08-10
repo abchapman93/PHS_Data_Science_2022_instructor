8db3058dfd961feae1313362b1dc4a96137badab
Alec Chapman <abchapman93@gmail.com>
2022-08-10 08:25:42 -0600

    Refactor student version of 1.3

--- quizzes/module_3_exercise_quizzes.py
+++ quizzes/module_3_exercise_quizzes.py
@@ -22,11 +22,32 @@ hint_mortality_by_race = QuizHint(hints=[
     """),
 ])
 
+hint_hematocrit_viz = QuizHint(hints=[
+    widgets.HTML("""Your output should look something like this: </br>
+    <img src="./media/hint_hematocrit_dist.png" width="50%"></img>""")
+])
+
 quiz_hematocrit_normal = MultipleChoiceQuiz("Among the 20,000 rows, how many have a 'Normal' result?", answer=11862,
                    options=[11862, 20000, 8088, 50])
 
 quiz_count_abi = MultipleChoiceQuiz("How many rows are in abi before/after dropping missing values?",
                   answer="364; 189", options=["10000; 5000", "2632; 2632"])
+
+hint_abi = QuizHint(hints=[
+    widgets.HTML("""We did something similar in <a href="./03-Labs-and-Vitals.ipynb" target="_blank">03-Labs-and-Vitals</a>
+    with systolic and diastolic blood pressure."""),
+                widgets.HTML("""In a histogram, a larger number of bins shows more detail, a smaller number shows less. 
+                Here are two examples of histograms for abi with different numbers of bins:</br>
+                <img src="./media/hint_abi_hist.png" width="50%"></img>""")
+])
+
+hint_prevalence_by_race = QuizHint(hints=[
+    widgets.HTML("""To do this, you'll need to use two different subqueries (one to get the total counts by ethnicity,
+    another to get the counts of each disease by ethnicity) and join them together."""),
+    widgets.HTML("""The first subquery should group by ethnicity_descr, the second should group by ethnicity_descr and ICD-9 code."""),
+    widgets.HTML("""Here is a visualization showing the results: </br>
+    <img src="./media/hint_prop_disease_by_race.png" width="50%"></img>""")
+])
 
 quiz_dx_prevalence_by_race = SelectMultipleQuiz(
     "Which of the diseases in question appear to be more prevalent among patients who are Black/African American?",
